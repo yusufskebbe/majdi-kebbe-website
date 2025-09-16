0363af175cd49e2397d6e9dbc966f8a64b3a1193
Yusuf Kebbe <yusuf.kebbe@paytr.com>
2025-09-17 00:59:01 +0300

    feat: project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, signal, effect, DestroyRef } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'angular-tailwind-starter';
+  http = inject(HttpClient);
+  destroyRef = inject(DestroyRef);
+  fb = inject(FormBuilder);
+  private toast = inject(ToastrService);
+
+  constructor() {
+    // Effect to handle body scroll locking
+    effect(() => {
+      if (this.isMobileMenuOpen()) {
+        document.body.classList.add("menu-open");
+      } else {
+        document.body.classList.remove("menu-open");
+      }
+    });
+
+    // Cleanup on component destroy
+    this.destroyRef.onDestroy(() => {
+      document.body.classList.remove("menu-open");
+    });
+  }
+
+  // Mobile menu state
+  isMobileMenuOpen = signal(false);
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen.update((value) => !value);
+  }
+
+  // Course carousel methods
+  scrollLeft() {
+    const container = document.querySelector(".course-scroll-container");
+    if (container) {
+      container.scrollBy({ left: -320, behavior: "smooth" });
+    }
+  }
+
+  scrollRight() {
+    const container = document.querySelector(".course-scroll-container");
+    if (container) {
+      container.scrollBy({ left: 320, behavior: "smooth" });
+    }
+  }
+
+  redirectToWhatsApp() {
+    window.open(
+      "https://api.whatsapp.com/send?phone=905340798944&text=Merhaba%20bilgi%20alabilir%20miyim%20%3F",
+      "_blank"
+    );
+  }
+
+  contactForm = this.fb.group({
+    name: ["", [Validators.required]],
+    email: ["", [Validators.required, Validators.email]],
+    phoneNumber: ["", [Validators.required]],
+  });
+
+  onSubmit() {
+    this.http
+      .post("https://formspree.io/f/xnnpbogo", this.contactForm.value)
+      .subscribe({
+        next: (res) => {
+          this.toast.success("Form başarıyla gönderildi!", "Başarılı");
+          this.contactForm.reset();
+        },
+        error: (error) => {
+          this.toast.error(
+            "Form gönderilirken bir hata oluştu. Lütfen tekrar deneyin.",
+            "Hata"
+          );
+          console.error("Form submission error:", error);
+        },
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NgxNumberTickerComponent } from "@omnedia/ngx-number-ticker";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { ToastrModule } from "ngx-toastr";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgxNumberTickerComponent,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: "toast-top-right",
+      preventDuplicates: true,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
